aa48686f4eb6975dc670f6a27cd4d93c8665fdc4
Luiz Campos <campos.antoonio@gmail.com>
2024-11-19 18:28:29 -0300

    feat(auth): remove old jwt code

--- internal/auth/auth.go
+++
@@ -1,8 +0,0 @@
-package auth
-
-import "github.com/golang-jwt/jwt/v5"
-
-type Authenticator interface {
-	GenerateToken(subject int64) (string, error)
-	ValidateToken(token string) (*jwt.Token, error)
-}
